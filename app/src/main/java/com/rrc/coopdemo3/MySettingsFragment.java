bb645ebfc86c58f1041a8347da4c30809698f8aa
farid <nzhang2@academic.rrc.ca>
2022-11-06 15:40:29 -0600

    Add sharedPreferences changeListener.

--- app/src/main/java/com/rrc/coopdemo3/MySettingsFragment.java
+++ app/src/main/java/com/rrc/coopdemo3/MySettingsFragment.java
@@ -1,16 +1,21 @@
 package com.rrc.coopdemo3;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class MySettingsFragment extends PreferenceFragmentCompat {
+
+    private SharedPreferences.OnSharedPreferenceChangeListener listener;
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
@@ -44,5 +49,29 @@ public class MySettingsFragment extends PreferenceFragmentCompat {
             });
         }
 
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if (key.equals("notifications")) {
+                    boolean are_notifications_enabled =  sharedPreferences.getBoolean(key, false);
+                    Toast.makeText(getActivity(), "notifications changed to: " + are_notifications_enabled, Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
     }
 }
